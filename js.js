4ec3dcee30cd14b85df0ee1b9ddffcbaa7ae87d4
beketamas <72067154+beketamas@users.noreply.github.com>
2024-01-03 12:51:40 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,10 +1,22 @@
+let intervalID = null
+
 function Forgatas(){
+    if(intervalID != null)
+        return
+
     let startDate = new Date()
-    setInterval(function(){
+    intervalID = setInterval(function(){
         let date = new Date()
-        document.getElementById("outCircle").style.rotate = `${360 / 20 * parseInt((date - startDate) / 1000)}deg`
-        document.getElementById("timer").style.rotate = `-${360 / 20 * parseInt((date - startDate) / 1000)}deg`
+        let sec =  parseInt((date - startDate) / 1000)
+        document.getElementById("outCircle").style.rotate = `${360 / 20 * sec}deg`
+        //document.getElementById("timer").style.rotate = `-${360 / 20 * parseInt((date - startDate) / 1000)}deg`
+        document.getElementById("timer").innerHTML = `00:${String(20 - sec).padStart(2, '0')}`;
+        if(sec >= 20){
+            clearInterval(intervalID)
+            intervalID = null
+        }
     },100)
+    location.reload();
 }
 function Reset(){
     location.reload();
