6114a684833e13d81d8654ba2d483220c18f0c61
beketamas <72067154+beketamas@users.noreply.github.com>
2024-01-03 13:06:08 +0100

    Add files via upload

--- js.js
+++ js.js
@@ -1,4 +1,5 @@
 let intervalID = null
+let time = parseInt(document.getElementById("timer").innerHTML.split(':')[1])
 
 function Forgatas(){
     if(intervalID != null)
@@ -8,10 +9,10 @@ function Forgatas(){
     intervalID = setInterval(function(){
         let date = new Date()
         let sec =  parseInt((date - startDate) / 1000)
-        document.getElementById("outCircle").style.rotate = `${360 / 20 * sec}deg`
+        document.getElementById("outCircle").style.rotate = `${360 / time * sec}deg`
         //document.getElementById("timer").style.rotate = `-${360 / 20 * parseInt((date - startDate) / 1000)}deg`
-        document.getElementById("timer").innerHTML = `00:${String(20 - sec).padStart(2, '0')}`;
-        if(sec >= 20){
+        document.getElementById("timer").innerHTML = `00:${String(time - sec).padStart(2, '0')}`;
+        if(sec >= time){
             clearInterval(intervalID)
             intervalID = null
         }
